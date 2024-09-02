a047662446658e7c70e685299636b53eb43064f4
May <2025.may.fujita@uwcisak.jp>
2024-09-03 08:25:43 +0900

    Quiz 077

--- quiz_files/quiz_075.py
+++ quiz_files/quiz_075.py
@@ -1,4 +1,4 @@
-# Quiz 075 & 076
+# Quiz 075
 from matplotlib import pyplot as plt
 
 def find_num_of_parity_bits(len_msg:int) -> int:
@@ -20,9 +20,24 @@ plt.ylabel("Efficiency", fontsize=15)
 plt.show()
 
 
+# Quiz 076
 def find_parity_indices(k: int) -> list[int]:
     positions = []
     for n in range(k):
         positions.append((2**n)-1)
     return positions
 
+
+# Quiz 077
+def get_indices_checked(p: int, n: int, k: int) -> list[int]:
+    """Gets the indices of the bits checked by the p-th parity bit.
+    p: int, the p-th parity bit
+    n: int, the length of the message intending to be sent
+    k: int, the number of parity bits
+    """
+
+    indices = []
+    for a in range(n+k+1):
+        if a & (2**(p-1)):
+            indices.append(a-1)
+    return indices
